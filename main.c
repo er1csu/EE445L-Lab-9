78ea6b7e7b3bf8c44e6d872f1a2ae2ece90e0ccc
ericsu <ekeen4@gmail.com>
2016-11-06 00:30:04 -0500

    Initial commit

---
+++ main.c
@@ -0,0 +1,78 @@
+// main.c
+// Runs on LM4F120/TM4C123
+// UART runs at 115,200 baud rate 
+// Daniel Valvano
+// May 3, 2015
+
+/* This example accompanies the books
+  "Embedded Systems: Introduction to ARM Cortex M Microcontrollers",
+  ISBN: 978-1469998749, Jonathan Valvano, copyright (c) 2015
+
+"Embedded Systems: Real Time Interfacing to ARM Cortex M Microcontrollers",
+   ISBN: 978-1463590154, Jonathan Valvano, copyright (c) 2015
+ 
+ Copyright 2015 by Jonathan W. Valvano, valvano@mail.utexas.edu
+    You may use, edit, run or distribute this file
+    as long as the above copyright notice remains
+ THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
+ OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
+ VALVANO SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL,
+ OR CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
+ For more information about my classes, my research, and my books, see
+ http://users.ece.utexas.edu/~valvano/
+ */
+
+
+#include <stdint.h> // C99 variable types
+#include "../inc/tm4c123gh6pm.h"
+#include "ADCSWTrigger.h"
+#include "uart.h"
+#include "PLL.h"
+
+long StartCritical (void);    // previous I bit, disable interrupts
+void EndCritical(long sr);    // restore I bit to previous value
+
+void Timer0A_Init(uint32_t period);
+
+int numSamples;
+
+int main(void){
+  PLL_Init(Bus80MHz);   // 80 MHz
+  Timer0A_Init(80000);
+  UART_Init();              // initialize UART device
+  ADC0_InitSWTriggerSeq3_Ch9();
+  while(1){   
+  }
+}
+
+// ***************** Timer0A_Init ****************
+// Activate TIMER0 interrupts to run user task periodically
+// Inputs:  task is a pointer to a user function
+//          period in units (1/clockfreq), 32 bits
+// Outputs: none
+void Timer0A_Init(uint32_t period){
+    long sr;
+    numSamples = 0;
+    sr = StartCritical(); 
+    SYSCTL_RCGCTIMER_R |= 0x01;   // 0) activate TIMER0
+    TIMER0_CTL_R = 0x00000000;    // 1) disable TIMER0A during setup
+    TIMER0_CFG_R = 0x00000000;    // 2) configure for 32-bit mode
+    TIMER0_TAMR_R = 0x00000002;   // 3) configure for periodic mode, default down-count settings
+    TIMER0_TAILR_R = period-1;    // 4) reload value
+    TIMER0_TAPR_R = 0;            // 5) bus clock resolution
+    TIMER0_ICR_R = 0x00000001;    // 6) clear TIMER0A timeout flag
+    TIMER0_IMR_R = 0x00000001;    // 7) arm timeout interrupt
+    NVIC_PRI4_R = (NVIC_PRI4_R&0x00FFFFFF)|0x80000000; // 8) priority 4
+    // interrupts enabled in the main program after all devices initialized
+    // vector number 35, interrupt number 19
+    NVIC_EN0_R = 1<<19;           // 9) enable IRQ 19 in NVIC
+    TIMER0_CTL_R = 0x00000001;    // 10) enable TIMER0A
+    EndCritical(sr);
+}
+
+void Timer0A_Handler(void) {
+    int32_t data = ADC0_InSeq3();
+    // Output to FIFO
+}
+
